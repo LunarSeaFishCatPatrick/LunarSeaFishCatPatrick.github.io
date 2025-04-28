a4f33e516df2a4404e0907a468edec3f4d93663c
LunarSeaFishCatPatrick <trickster7840@gmail.com>
2025-04-28 22:32:42 +0800

    Update Cscript.js

--- FinalProject/javascripts/Cscript.js
+++ FinalProject/javascripts/Cscript.js
@@ -41,3 +41,50 @@ clorindeMusicVideoButton.onclick = function () {
   clorindeGallerySection.style.display = "none";
   clorindeMusicVideoSection.style.display = "block";
 };
+const modal = document.getElementById("imageModal");
+const modalImg = document.getElementById("modalImage");
+const closeModal = document.querySelector(".close");
+const prevBtn = document.querySelector(".prev-btn");
+const nextBtn = document.querySelector(".next-btn");
+
+const galleryImages = Array.from(
+  document.querySelectorAll("#CGallery .gallery-grid img")
+).slice(0, 8);
+
+let currentImgIndex = 0;
+
+galleryImages.forEach((img, index) => {
+  img.addEventListener("click", () => {
+    currentImgIndex = index;
+    openModal();
+  });
+});
+
+function openModal() {
+  modal.style.display = "flex";
+  modalImg.src = galleryImages[currentImgIndex].src;
+}
+
+function showNextImage() {
+  currentImgIndex = (currentImgIndex + 1) % galleryImages.length;
+  openModal();
+}
+
+function showPrevImage() {
+  currentImgIndex =
+    (currentImgIndex - 1 + galleryImages.length) % galleryImages.length;
+  openModal();
+}
+
+nextBtn.addEventListener("click", showNextImage);
+prevBtn.addEventListener("click", showPrevImage);
+
+closeModal.addEventListener("click", () => {
+  modal.style.display = "none";
+});
+
+modal.addEventListener("click", (e) => {
+  if (e.target === modal) {
+    modal.style.display = "none";
+  }
+});
